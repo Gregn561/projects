e2d75d9c0616babfca7e50d4a87ba314f3a7718b
Gregn561 <greg.nelson1223@gmail.com>
2026-04-19 19:54:09 -0400

    new py file

---
+++ creditcardtrans/creditccardq1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df= pd.read_csv("creditcardtrans/credit_card_transQ1.csv")
+df= pd.DataFrame(df)
+
+x= df['category']
+y= df['amount']
+
+def plot(x, y, color="blue"):
+    plt.bar(x, y, color = color)
+    plt.xlabel("category")
+    plt.ylabel("amount")
+    plt.title("spending by category")
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.show()
+
+plot(x,y)
