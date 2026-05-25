cd57f71823069cb29d6eaf4f5d1ddcc4d684581b
Gregn561 <greg.nelson1223@gmail.com>
2026-05-25 18:24:16 -0400

    final regression update

--- Python Lab/regression.py
+++ Python Lab/regression.py
@@ -79,10 +79,122 @@ price3 = lm4.intercept_ + lm4.coef_[0] * z1['normalized-losses'] + lm4.coef_[1] 
 
 width = 12 
 height = 10
-plt.figure(figsize=(width, height))
-sns.residplot(df['highway-mpg'], df['price'])
-plt.show()
+# plt.figure(figsize=(width, height))
+# sns.residplot(x=df['highway-mpg'], y=df['price'])
+# plt.show()
+z2 = df[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg']]
+lm5 = LinearRegression()
+lm5.fit(z2, df['price'])
+yhat = lm5.predict(z2)
 
+# plt.figure(figsize=(width, height))
+# sns.kdeplot(df['price'], label='Actual Values', color='blue')
+# sns.kdeplot(yhat, label='Fitted Values', color='red')
+# plt.title('Actual vs Fitted Values for Price')
+# plt.xlabel('Price (in dollars)')
+# plt.ylabel('Proportion of Cars')
+# plt.show()
+# plt.close()
+
+# polynominal regression and pipelines
+
+def plotpolly(model, independent_variable, dependent_variable, name):
+    x_new = np.linspace(15,55,100)
+    y_new = model(x_new)
+
+    plt.plot(independent_variable, dependent_variable, '.', x_new, y_new, '-')
+    plt.title('ploynomial fit with matplotlib for price ~ length')
+    ax = plt.gca()
+    ax.set_facecolor((0.898, 0.898, 0.898))
+    fig = plt.gcf()
+    plt.xlabel(name)
+    plt.ylabel('price of cars')
+    plt.show()
+    plt.close()
+
+
+x = df['highway-mpg']
+y = df['price']
+f = np.polyfit(x, y, 3)
+p = np.poly1d(f)
+# plotpolly(p, x, y, 'highway-mpg')
+
+#multivariate polynominal regression
+from sklearn.preprocessing import PolynomialFeatures
+
+# pr = PolynomialFeatures(degree=2)
+# z_pr = pr.fit_transform(z) 
+
+# print(z.shape)
+# print(z_pr.shape)
+
+#pipelines
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+# input = [(('scale', StandardScaler())), ('polynomial', PolynomialFeatures(include_bias=False)), ('model', LinearRegression())]  # create a list of tuples including scaling, polynomial transform and linear regression steps
+# pipe=Pipeline(input)    # create a pipeline object with the list as argument
+
+# z=z.astype(float)   # convert to float to avoid error with StandardScaler
+# pipe.fit(z,y)   # fit the pipeline with the features and target variable
+
+# ypipe = pipe.predict(z) # predict the target variable using the pipeline
+# print('the predicted value is:', ypipe[0:4])
+
+# input1 = [('scale1', StandardScaler()), ('model1', LinearRegression())]
+
+# pipe1=Pipeline(input1)
+# z1 = z1.astype(float)
+
+# pipe1.fit(z1,y)   # fit the pipeline with the features and target variable
+# ypipe1 = pipe1.predict(z1)
+# print('the predicted value is:', ypipe1[0:6])
+
+#measures for in-sample evaluation of the model
+
+#model 1 simple linear
+#calculate R-squared, for highway-mpg
+# lm.fit(x,y)
+# print('the r square is:', lm.score(x,y))
+
+#calculate mean squared error(MSE) for highway-mpg
+# yhat = lm.predict(x)
+# print('the predicted values are:', yhat[0:4])
+
+from sklearn.metrics import mean_squared_error
+# mse = mean_squared_error(df['price'], yhat)
+# print('the mse is:', mse)
+
+#model 2 multi linear regression
+# lm.fit(z, df['price'])
+# print('the rsquare is:', lm.score(z, df['price']))
+
+# y_predict_multifit = lm.predict(z)
+# print('the predicted values are:', y_predict_multifit[0:4])
+
+#mse for multi linear regression
+# print('the mse is:', mean_squared_error(df['price'], y_predict_multifit))
+
+#model 3 polynomial regression
+from sklearn.metrics import r2_score
+
+r_squared = r2_score(y, p(x))
+# print('the r squared is:', r_squared)
+
+# mse for polynomial regression
+# print('the mse is:', mean_squared_error(df['price'], p(x)))
+
+#prediction and decision making
+x = df[['highway-mpg']]
+new_input = np.arange(1, 100, 1).reshape(-1, 1)
+#fit the model
+lm.fit(x, y)
+#predict the model
+yhat = lm.predict(new_input)
+print(yhat[0:5])
+
+plt.plot(new_input, yhat)
+plt.show()
 
 
 
